ce48ce3a517bfd2143ee68886bcc080f3464b92a
kimjinho1 <rlawlsgh8113@naver.com>
2022-06-24 15:51:51 +0900

    2022'06-24

--- 거리두기 확인하기/solution.py
+++ 거리두기 확인하기/solution.py
@@ -1,32 +1,30 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(10000)
 
 def solution(places):    
     def bfs(i, j):
-        q = deque([(i, j)])
+        q = [(i, j)]
         while q:
-            y, x = q.popleft()
+            y, x = q.pop(0)
             for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 Y, X = y+dy, x+dx
-                d = abs(Y-i) + abs(X-j)
-                if d <= 2 and (0 <= Y < 5) and (0 <= X < 5) and check_li[Y][X] == 0:
-                    check_li[Y][X] = 1
-                    if place[Y][X] == 'P':
-                        return 0
-                    elif place[Y][X] == 'O':
+                if 0 <= Y < 5 and 0 <= X < 5 and abs(Y-i) + abs(X-j) <= 2:
+                    if place[Y][X] == 'O':
+                        place[Y][X] = 'X'
                         q.append((Y, X))
+                    elif place[Y][X] == 'P':
+                        return 0
         return 1
     
     ans = []
     for place in places:
-        check_li = [[0]*5 for _ in range(5)]
+        place = list(map(list, place))
         check = 1
         for i in range(5):
             for j in range(5):
-                if place[i][j] == 'P' and check_li[i][j] == 0:
-                    check_li[i][j] = 1
-                    check = check and bfs(i, j)
-        ans.append(check)                    
+                if place[i][j] == 'P':
+                    place[i][j] = 'X'
+                    check &= bfs(i, j)
+        ans.append(check)                       
                     
     return ans
