d957431246043f268da82f44c8b7b9863dba84de
kimjinho1 <rlawlsgh8113@naver.com>
2022-06-26 00:18:59 +0900

    2022-06-26

--- 비밀지도/solution.py
+++ 비밀지도/solution.py
@@ -1,15 +1,7 @@
-def binary(num, n):
-    b = bin(num)[2:]
-    return (n - len(b))*'0' + b
-
-def str_sum(n, s1, s2):
-    s = ''
-    for i in range(n):
-        s += '#' if s1[i] == '1' or s2[i] == '1' else ' '
-    return s
-
 def solution(n, arr1, arr2):
-    ans = [0] * n
+    ans = []
     for i in range(n):
-        ans[i] = str_sum(n, binary(arr1[i], n), binary(arr2[i], n))
+        b = bin(arr1[i] | arr2[i])[2:]
+        ans.append((n - len(b))*' ' + b.replace('1', '#').replace('0', ' '))
+        
     return ans
